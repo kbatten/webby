9cbce60479435d39be67a5ecdb5db31815102105
Keith Batten <kbatten@gmail.com>
2019-10-18 02:39:21 -0700

    simple ease in out for center

--- webby.py
+++ webby.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+import math
 
 from mathics.world import World
 from mathics.viewport import Viewport
 from mathics.machines.basic import Machine, Point
 
 from gifserve import write_gif, serve_gif
-
 
 
 class Web(Machine):
@@ -19,15 +19,40 @@ class Web(Machine):
 
     def __init__(self):
         super(Web, self).__init__()
+        self.center = Point(50, 50)
 
-    def visualization(self, vp, data={}):
+    def visualization(self, vp):
         Viewport = vp
-        vp.add_object(Viewport.Circle(Point(50, 50), 5, Viewport.WHITE))
+
+        vp.add_object(Viewport.Text(Point(3,3), self.dyn_t, (170,130,170)))
+
+        # select center
+        vp.add_object(Viewport.Circle(self.center, self.dyn_select_center_point_rad, Viewport.WHITE))
+
+    def dyn_t(self):
+        return "%0.2f s" % self.t
+
+    def dyn_select_center_point(self):
+        return Point(50 + 10*math.sin(2*self.t), 50 + 10*math.cos(2*self.t))
+
+    def dyn_select_center_point_rad(self):
+        ret = 4.0
+        if self.t < 0.5:
+            t = 2.0 * self.t
+            ret = 1.0 + ret * self.ease_in_out_sin(t)
+        elif self.t < 1:
+            t = 2.0 * self.t - 1.0
+            ret = 1.0 + ret - self.ease_in_out_sin(t)
+        return ret
+
+    @staticmethod
+    def ease_in_out_sin(t):
+        return 0.5 + 0.5 * math.sin(t * math.pi - math.pi / 2.0)
 
 
 def setup_world(w, h, supersample):
     # create world
-    world = World(w, h, supersample=supersample)
+    world = World(w, h, font=("/usr/share/fonts/truetype/lato/Lato-Bold.ttf", 16), supersample=supersample)
 
     # create one viewport that is entire world
     viewport = Viewport(0, 0, 99, 99)
@@ -45,15 +70,18 @@ def main():
     WIDTH = 500
     HEIGHT = 500
     SUPERSAMPLE = 2
-
-    world = setup_world(WIDTH, HEIGHT, SUPERSAMPLE)
+    TIME_DURATION = 5
+    SECONDS_PER_FRAME = 0.05
 
     TIME_START_SEC = 0
-    TIME_DURATION_SEC = 5
-    SECONDS_PER_FRAME = 0.05
+    TIME_DURATION_SEC = SECONDS_PER_FRAME * math.ceil(TIME_DURATION / SECONDS_PER_FRAME)
+
+    world = setup_world(WIDTH, HEIGHT, SUPERSAMPLE)
     frames = world.get_frames(TIME_START_SEC, TIME_DURATION_SEC, SECONDS_PER_FRAME)
 
     gif = write_gif(frames, TIME_DURATION_SEC)
+
+    print("starting server")
     serve_gif(gif)
 
 
